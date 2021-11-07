37f6804afe9b291860d6e287212d18922d7f0191
MoBiEeW <mobiew55@gmail.com>
2021-11-08 00:22:16 +0700

    add ignore

--- src/main.java
+++ src/main.java
@@ -4,6 +4,8 @@ public class main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hi Project");
+		System.out.println("Hi Project");
+
 	}
 
 }
