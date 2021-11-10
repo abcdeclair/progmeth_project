5b598da0154195b36b17570cc43cb6b348af3fc3
MoBiEeW <mobiew55@gmail.com>
2021-11-10 19:34:21 +0700

    add menu_page

---
+++ src/GUI/Menu.java
@@ -0,0 +1,33 @@
+package GUI;
+
+import javafx.application.Application;
+
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Menu extends Application{
+	
+	public void start(Stage primaryStage) {
+		// TODO
+		Image imageview = new Image("testtest.png");
+		StackPane root = new StackPane();
+		Canvas canvas = new Canvas(1400,800);
+		GraphicsContext gc = canvas.getGraphicsContext2D();
+		root.getChildren().add(canvas);
+		gc.drawImage(imageview, 0, 0,1400,800);
+		
+		Scene scene = new Scene(root);
+	    primaryStage.setScene(scene);
+	    primaryStage.setTitle("Mobiews_Ocean");
+	    //primaryStage.setResizable(false);
+	    primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
