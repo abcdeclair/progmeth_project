9cf76f5ba900b828d96adfbc15a7f005bfabbcea
MoBiEeW <mobiew55@gmail.com>
2021-11-19 09:49:15 +0700

    fix manage map

--- src/GUI/Menu.java
+++ src/GUI/Menu.java
@@ -1,14 +1,23 @@
 package GUI;
 
+
+import javafx.scene.media.Media;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.media.AudioClip;
 import javafx.stage.Stage;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.layout.StackPane;
+import javafx.scene.media.AudioClip;
+import javafx.stage.Stage;
 
 public class Menu extends Application {
 
+	
 	public static final String bgm = "/originalsoundtrack.mp3";
 	public static AudioClip backgroundMusic;
 
@@ -19,12 +28,14 @@ public class Menu extends Application {
 		backgroundMusic.setCycleCount(AudioClip.INDEFINITE);
 		backgroundMusic.play();
 
+
 		Parent root = FXMLLoader.load(getClass().getResource("menuButton.fxml"));
 		Scene scene = new Scene(root);
 		primaryStage.setScene(scene);
 		primaryStage.setTitle("Mobiews_Ocean");
 		primaryStage.setResizable(false);
 		primaryStage.show();
+
 	}
 
 	public static void main(String[] args) {
