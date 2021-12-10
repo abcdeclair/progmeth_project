dc6e682d0dee8af664ec024cad5b4f16416b61f9
abcdeclair <abcdeclair@gmail.com>
2021-12-10 15:03:36 +0700

    play game button

--- src/GUI/MenuController.java
+++ src/GUI/MenuController.java
@@ -1,5 +1,6 @@
 package GUI;
 
+import javafx.animation.AnimationTimer;
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
@@ -10,6 +11,10 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import MainGame.RenderableHolder;
+import drawing.GameScreen;
+import gamelogic.GameLogic;
+import input.InputUtility;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -19,6 +24,7 @@ import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
+import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
@@ -31,12 +37,36 @@ public class MenuController extends VBox implements Initializable {
 	@FXML
 	private void PlaygameClick(ActionEvent event) throws IOException {
 
-		FXMLLoader loader = new FXMLLoader(getClass().getResource("settingTask.fxml"));
-		Parent root = loader.load();
+//		FXMLLoader loader = new FXMLLoader(getClass().getResource("settingTask.fxml"));
+//		Parent root = loader.load();
+//		Scene scene = new Scene(root);
+//		Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+//		app_stage.setScene(scene);
+//		app_stage.show();
+		StackPane root = new StackPane();
 		Scene scene = new Scene(root);
+//		stage.setScene(scene);
+//		stage.setTitle("Fish game");
+		((Node)(event.getSource())).getScene().getWindow().hide();
 		Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
 		app_stage.setScene(scene);
+
+		GameLogic logic = new GameLogic();
+		GameScreen gameScreen = new GameScreen(1400, 800);
+		root.getChildren().add(gameScreen);
+		gameScreen.requestFocus();
+		
 		app_stage.show();
+		
+		AnimationTimer animation = new AnimationTimer() {
+			public void handle(long now) {
+				gameScreen.paintComponent();
+				logic.logicUpdate();
+				RenderableHolder.getInstance().update();
+				InputUtility.updateInputState();
+			}
+		};
+		animation.start();
 
 	}
 
