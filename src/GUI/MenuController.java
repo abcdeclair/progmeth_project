0df90fc4b9865a948f4727ad91e12940eae5fcf2
abcdeclair <abcdeclair@gmail.com>
2021-12-10 12:36:56 +0700

    settingController added

--- src/GUI/MenuController.java
+++ src/GUI/MenuController.java
@@ -1,32 +1,41 @@
 package GUI;
 
-
 import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.control.Button;
+
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
-
-
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
-public class MenuController extends VBox{
-	
+public class MenuController extends VBox implements Initializable {
+
 	@FXML
 	public Button playgameButton;
 
 	@FXML
-	private void PlaygameClick() {
-		
-		System.out.println("Hello");
+	private void PlaygameClick(ActionEvent event) throws IOException {
+
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("settingTask.fxml"));
+		Parent root = loader.load();
+		Scene scene = new Scene(root);
+		Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+		app_stage.setScene(scene);
+		app_stage.show();
 
 	}
 
@@ -34,7 +43,17 @@ public class MenuController extends VBox{
 	public Button settingButton;
 
 	@FXML
-	private void SettingClick() {
+	private void SettingClick(ActionEvent event) throws IOException {
+
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("settingTask.fxml"));
+		Parent root = loader.load();
+
+		SettingController settingControl = loader.getController();
+		Scene scene = new Scene(root);
+		Stage app_stage = new Stage();
+		app_stage.setScene(scene);
+		app_stage.setResizable(false);
+		app_stage.show();
 
 	}
 
@@ -52,6 +71,12 @@ public class MenuController extends VBox{
 	@FXML
 	public void ExitClick(ActionEvent event) {
 		Platform.exit();
+
+	}
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		// TODO Auto-generated method stub
 
 	}
 
---
+++ src/GUI/SettingController.java
@@ -0,0 +1,26 @@
+package GUI;
+
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.layout.StackPane;
+
+public class SettingController extends StackPane{
+	
+	@FXML
+	public Button backToMainButton;
+	
+	@FXML
+	public CheckBox musicCheckBox;
+	
+	@FXML
+	public CheckBox soundCheckBox;
+	
+	@FXML
+	private void backToMainMenu() {
+
+		
+	}
+
+}
